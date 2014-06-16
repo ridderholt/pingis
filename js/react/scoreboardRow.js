f3a6e62f6c93603f3706d9ec3ff13c6c680c6782
Robin Ridderholt <robin.ridderholt@live.com>
2014-06-16 22:38:45 +0200

    Extracts footer to a template

--- js/react/scoreboardRow.js
+++ js/react/scoreboardRow.js
@@ -3,25 +3,12 @@
 
 var ScoreboardRow = React.createClass({displayName: 'ScoreboardRow',
 	render: function () {
-
-		var imgDivStyle = {
-			borderRadius: '50px',
-			position: 'relative',
-			overflow: 'hidden',
-			width: '100px',
-			height: '100px'
-		};
-
-		var imgStyle = {
-			width: '100px'
-		};
-
 		return (
 				React.DOM.tr(null, 
 					React.DOM.td(null, "#",this.props.data.position),
 					React.DOM.td(null, 
-						React.DOM.div( {style:imgDivStyle}, 
-							React.DOM.img( {style:imgStyle, src:this.props.data.imageUrl} )
+						React.DOM.div( {className:"img-container"}, 
+							React.DOM.img( {src:this.props.data.imageUrl} )
 						)
 					),
 					React.DOM.td(null, 
--- js/scoreboardRow.js
+++ js/scoreboardRow.js
@@ -3,25 +3,12 @@
 
 var ScoreboardRow = React.createClass({
 	render: function () {
-
-		var imgDivStyle = {
-			borderRadius: '50px',
-			position: 'relative',
-			overflow: 'hidden',
-			width: '100px',
-			height: '100px'
-		};
-
-		var imgStyle = {
-			width: '100px'
-		};
-
 		return (
 				<tr>
 					<td>#{this.props.data.position}</td>
 					<td>
-						<div style={imgDivStyle}>
-							<img style={imgStyle} src={this.props.data.imageUrl} />
+						<div className="img-container">
+							<img src={this.props.data.imageUrl} />
 						</div>
 					</td>
 					<td>
